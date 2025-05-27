82e4a440e42f462d4e0fcbfaaf981fb7bc2adf1a
D4rkgh057 <danielfuelpaz@outlook.com>
2025-05-27 16:42:44 -0500

    feat: agregar ruta protegida y componente ProtectedRoute para gestionar acceso a rutas

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Users from './pages/Users';
 import Login from './pages/Login';
 import Sidebar from './components/Sidebar';
 import Companies from './pages/Companies';
+import NotFound from './pages/NotFound';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const MainLayout: React.FC = () => (
   <div className="flex min-h-screen bg-gray-50">
@@ -14,6 +16,7 @@ const MainLayout: React.FC = () => (
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/users" element={<Users />} />
         <Route path="/companies" element={<Companies />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </main>
   </div>
@@ -26,7 +29,10 @@ const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/*" element={isLogin ? <Login /> : <MainLayout />} />
+      <Route element={<ProtectedRoute />}> {/* Protege todas las rutas excepto login */}
+        <Route path="/*" element={isLogin ? <Login /> : <MainLayout />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
